3f9f4efe682f63841a20e2f971b4edaa36c9d0f3
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-11-24 03:48:12 +0530

    frontpage

--- Dashboard/src/pages/home/home.jsx
+++ Dashboard/src/pages/home/home.jsx
@@ -3,13 +3,37 @@ import styles from "./home.module.css";
 export default function Home() {
   return (
     <>
+      <div className={styles.hero}>
+        <div className={styles.hero_content_left}>
+          <input type="text" placeholder="   Search..." />
+        </div>
+        <div className={styles.hero_content_right}>
+          <button className={`${styles.button} ${styles.create}`} role="button">
+            Add Asset
+          </button>
+          <button className={`${styles.button} ${styles.create}`} role="button">
+            Sort
+          </button>
+        </div>
+      </div>
       <div className={styles.container}>
         <div className={styles.card}>
           <div className={styles.inner_left}>
             <h1>Asset Name</h1>
           </div>
           <div className={styles.inner_right}>
-            <h1>Home page content</h1>
+            <div className={`${styles.inner_right_one} ${styles.inner_right_content}`}>
+              <h4 className={styles.inner_right_text_heading}>
+                Product Purchased
+              </h4>
+              <h5 className={styles.inner_right_text_data}>06 October 2022</h5>
+            </div>
+            <div className={`${styles.inner_right_two} ${styles.inner_right_content}`}>
+              <h4 className={styles.inner_right_text_heading}>
+                Product Purchased
+              </h4>
+              <h5 className={styles.inner_right_text_data}>06 October 2022</h5>
+            </div>
           </div>
         </div>
       </div>
